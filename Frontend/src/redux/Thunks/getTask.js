91088f7de6c28e55532d089d210b109843a6fd45
muhammad-rizwan-brainx <muhammad.rizwan@brainxtech.com>
2023-08-10 18:04:17 +0500

    added axios configuration file

--- Frontend/src/redux/Thunks/getTask.js
+++ Frontend/src/redux/Thunks/getTask.js
@@ -1,9 +1,9 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import AXIOS from '../../axios.config'
 
 export const getTask = createAsyncThunk("posts", async () => {
   try {
-    const response = await axios.get("http://localhost:8000/api/v1/tasks/");
+    const response = await AXIOS.get("/tasks/");
     console.log(response.data);
     return response.data;
   } catch (error) {
